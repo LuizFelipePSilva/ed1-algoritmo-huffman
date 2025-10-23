56cf866a0aa514abdd71aa401229b9465dd4cc8c
LuizFelipePSilva <LuizFelipeP.S@outlook.com.br>
2025-10-23 15:35:40 -0300

    feat: add initial files

---
+++ ed1/src/HuffmanTree.java
@@ -0,0 +1,12 @@
+abstract class HuffmanTree implements Comparable<HuffmanTree> {
+    public final int frequency;
+
+    HuffmanTree(int frequency) {
+        this.frequency = frequency;
+    }
+
+    @Override
+    public int compareTo(HuffmanTree huffmanTree) {
+        return frequency - huffmanTree.frequency;
+    }
+}
